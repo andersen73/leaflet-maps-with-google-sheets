e7fb7771c4a128f645a904cc97d899bd914eb9bd
Ryan Andersen <andersen73@gmail.com>
2020-04-29 09:14:52 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nX2Kjxsx6VX7gInGUrpXzgOtkCiZTGiPiCYudKYB4eY/edit#gid=164271551';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1nX2Kjxsx6VX7gInGUrpXzgOtkCiZTGiPiCYudKYB4eY/edit#gid=0';
